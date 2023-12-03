6f407a1a2a8ba3613b1f41e3605d3cf142513228
Yuan Wang <gityw2021mbp13@yuanagain.com>
2023-12-03 08:14:51 -0500

    add timestamp to js sdk

--- packages/shorthandai-js/index.ts
+++ packages/shorthandai-js/index.ts
@@ -14,7 +14,59 @@ export type SHValueMatrix = SHValueScalar[][]
 
 export type SHValue = SHValueScalar | SHValueVector | SHValueMatrix
 
+export type SHValueAuthor = {
+  uid?: string
+  name?: string
+  email?: string
+}
+
+
+export type SHValueDoc = {
+  value: SHValue,
+  createdTS: number,
+  updatedTS: number,
+  author: SHValueAuthor,
+  description: string,
+}
+
 export const ShorthandValue = ({ token }: ShorthandDataAccessProps) => {
+
+  const getinfo = async (topicName: string, tag?: string) => {
+    try {
+      const res = await fetch(
+        `${__API_ROOT_URL__}/get`,
+        {
+          body: JSON.stringify({
+            topicName,
+            tag,
+            token,
+          }),
+          method: 'POST',
+          redirect: 'follow',
+          headers: [[ "Content-Type", "application/json" ]]
+        }
+      )
+  
+      if (res.status >= 500) {
+        throw new Error(res.statusText)
+      }
+      if (res.status >= 400) {
+        throw new Error(res.statusText)
+      }
+      const data = await res.json()
+      const value = {
+        value: data?.value,
+        createdTS: data?.createdTS,
+        updatedTS: data?.updatedTS,
+        author: data?.author,
+        description: data?.description,
+      } as SHValueDoc
+      return value
+    } catch (e) {
+      console.error(e)
+      return undefined
+    }
+  }
 
   const get = async (topicName: string, tag?: string) => {
     try {
@@ -77,6 +129,7 @@ export const ShorthandValue = ({ token }: ShorthandDataAccessProps) => {
   }
 
   return ({
+    getinfo,
     get,
     set,
   })
